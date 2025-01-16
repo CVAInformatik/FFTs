c8e2dff930cacd3384d991015158ef10562d1e3b
CVAInformatik <159441700+CVAInformatik@users.noreply.github.com>
2025-01-16 17:07:21 +0100

    Tabbing adjusted

--- simpleRadix2IOIPFFT.h
+++ simpleRadix2IOIPFFT.h
@@ -1,5 +1,5 @@
 /*
-Copyright  © 2025 Claus Vind-Andreasen
+Copyright  Â© 2025 Claus Vind-Andreasen
 
 This program is free software; you can redistribute it and /or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
 This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the GNU General Public License for more details.
@@ -53,7 +53,7 @@ public:
  		    for(u32 i = 0; i < length/2; i++) {
 					wre[i] = std::cos(( 2*PI *i )/length);
 					wim[i] = std::sin(( 2*PI *i )/length);
-    		}
+    			}
 		 }
 	}
 	
@@ -69,7 +69,7 @@ public:
 		     	 re[i] = re[i]/length;
 		     	 im[i] = im[i]/length;
 		     }
-  }
+	}
 	
 	private: 
 		u32 length ;
@@ -77,12 +77,12 @@ public:
 		FFTType *wim ;
 
 		void radix2IOIPFFT(u32 N,FFTType *re, FFTType*im, FFTType *wre, FFTType* wim  )
-    {
- 	    u32 NH = N/2;
-      u32 M = 1 ;
-      u32 mtemp = 1 ;
+    		{
+ 	   		u32 NH = N/2;
+      			u32 M = 1 ;
+      			u32 mtemp = 1 ;
 			while (mtemp < N) {mtemp = mtemp+ mtemp; M++;}
-  		M--;
+  				M--;
   
 			for( u32 L = 1; L < ((M+3)/2) ; L++ ) //30  Temperton's FORTRAN Label
 			{
@@ -124,14 +124,14 @@ public:
 				 	  		re[IB+I] = Zre;
 				 	  		im[IB+I] = Zim;
 				 			}
- 				  }// 10
-			  	KK = KK + LA;
+ 				  	}// 10
+			  		KK = KK + LA;
 		 		}//20
 			}//30
-	   for( u32 L = (M+3)/2; L <= M ; L++ ) // 70
+	   		for( u32 L = (M+3)/2; L <= M ; L++ ) // 70
 		 	{
 				u32 LA = 1;
-	  		for( u32 i = 0; i < (L-1); i++) LA = LA+LA;
+	  			for( u32 i = 0; i < (L-1); i++) LA = LA+LA;
 				u32 IA = 0;
 				u32 NH = N/2;
 				u32 IB = NH/LA;
@@ -156,10 +156,10 @@ public:
 		 	  				tim =  im[IC+I] - im[ID+I];
 		 	  				re[ID+I] = tre;
 		 	  				im[ID+I] = tim;
-          			re[IC+I] = Zre ;
-          			im[IC+I] = Zim ;
-  			  		}
-							else if( KK== (N/4)){ // W = (0,1)
+          						re[IC+I] = Zre ;
+          						im[IC+I] = Zim ;
+  			  			}
+						else if( KK== (N/4)){ // W = (0,1)
 		 	  				FFTType Zre = - ( im[IA+I] - im[IB+I]);
 		 	  				FFTType Zim =  re[IA+I] - re[IB+I];
 		 	  				re[IA+I] = re[IA+I] + re[IB+I];
@@ -171,10 +171,10 @@ public:
 		 	  				tim =   re[IC+I] - re[ID+I];
 		 	  				re[ID+I] = tre;
 		 	  				im[ID+I] = tim;
-          			re[IC+I] = Zre ;
-          			im[IC+I] = Zim ;
-							}
-							else {
+          						re[IC+I] = Zre ;
+          						im[IC+I] = Zim ;
+						}
+						else {
 					 	  	FFTType Wimag =   wim[KK];
 		 				  	FFTType Wreal =   wre[KK];
 		 	  				FFTType Zre = (Wreal*( re[IA+I] - re[IB+I])) - (Wimag* ( im[IA+I] - im[IB+I]));
@@ -188,12 +188,12 @@ public:
 		 	  				tim =  (Wimag*( re[IC+I] - re[ID+I])) + (Wreal* ( im[IC+I] - im[ID+I]));
 		 	  				re[ID+I] = tre;
 		 	  				im[ID+I] = tim;
-          			re[IC+I] = Zre ;
-          			im[IC+I] = Zim ;
-          		}
-		 	  		}//40
-		 	  	}// 50			
-		 	  	KK = KK + LA;
+          						re[IC+I] = Zre ;
+          						im[IC+I] = Zim ;
+          					}
+		 	  			}//40
+		 	  		}// 50			
+		 	  		KK = KK + LA;
 				}//60
 			}//70
 		}	
